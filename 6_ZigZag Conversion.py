93d266311c6cdf297f5e36a49a2da8ba020f8856
yancy <1014833992@163.com>
2018-01-11 19:43:52 +0800

    ZigZag Conversion
    ZigZag Conversion Solution

---
+++ 6_ZigZag Conversion.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+#https://leetcode.com/problems/zigzag-conversion/description/
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows == 1:
+            return s
+        n = 2 * numRows - 2
+        ret = []
+        lists = list(s)
+        lens = len(s)
+        ret.extend(lists[::n])
+        for i in range(1, numRows - 1):
+            j, k = i, n - 2 * i
+            while j < lens:
+                ret.append(lists[j])
+                j += k
+                k = n - k
+        ret.extend(lists[numRows - 1::n])
+        return ''.join(ret)
