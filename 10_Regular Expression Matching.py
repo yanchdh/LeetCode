32131b13c5e0971556984cae5bede6eb5a931f21
yancy <1014833992@163.com>
2018-01-18 10:21:32 +0800

    Update 10_Regular Expression Matching.py

--- 10_Regular Expression Matching.py
+++ 10_Regular Expression Matching.py
@@ -7,38 +7,31 @@ class Solution(object):
         :type p: str
         :rtype: bool
         """
-        self.s = s
-        self.p = p
-        self.lens = len(s)
-        self.lenp = len(p)
-        self.dp = [[-1 for i in range(self.lenp + 1)] for j in range(self.lens + 1)]
-        return self.Match(0, 0) == 1
+        self.ls, self.lp = len(s), len(p)
+        self.dp = [[-1 for i in range(self.lp + 1)] for j in range(self.ls + 1)]
+        return self.Match(s, p, 0, 0) == 1
     
-    def Match(self, i, j):
-        if j == self.lenp:
-            return 1 if i == self.lens else 0
+    def Match(self, s, p, i, j):
+        if j == self.lp:
+            return 1 if i == self.ls else 0
         
         if self.dp[i][j] != -1:
             return self.dp[i][j]
         
         ret = 0
-        if self.p[j] == '.':
-            if j + 1 < self.lenp and self.p[j + 1] == '*':
-                for k in range(i, self.lens + 1):
-                    if self.Match(k, j + 2):
-                        ret = 1
-                        break
-            else:
-                ret = i < self.lens and self.Match(i + 1, j + 1)
-        elif self.p[j] == '*':
-            if self.Match(i, j + 1) or \
-                (j > 0 and i < self.lens and self.s[i] == self.p[j - 1] and (self.Match(i + 1, j) or self.Match(i + 1, j + 1))):
+        if p[j] == '.' and j + 1 < self.lp and p[j + 1] == '*':
+            for k in range(i, self.ls + 1):
+                if self.Match(s, p, k, j + 2): 
+                    ret = 1
+                    break
+        elif j + 1 < self.lp and p[j + 1] == '*':
+            if self.Match(s, p, i, j + 2) or (i < self.ls and s[i] == p[j] and self.Match(s, p, i + 1, j + 1)):
                 ret = 1
-        elif j + 1 < self.lenp and self.p[j + 1] == '*':
-            if self.Match(i, j + 2) or (i < self.lens and self.s[i] == self.p[j] and self.Match(i + 1, j + 1)):
+        elif p[j] == '*':
+            if self.Match(s, p, i, j + 1) or (j > 0 and i < self.ls and s[i] == p[j - 1] and (self.Match(s, p, i + 1, j) or self.Match(s, p, i + 1, j + 1))):
                 ret = 1
-        else:
-            if i < self.lens and self.s[i] == self.p[j] and self.Match(i + 1, j + 1):
+        elif i < self.ls and (p[j] == '.' or s[i] == p[j]):
+            if self.Match(s, p, i + 1, j + 1):
                 ret = 1
         self.dp[i][j] = ret
         return ret
