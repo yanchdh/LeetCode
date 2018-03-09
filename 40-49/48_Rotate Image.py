e14bb911f5b84190ead8ea12b4a7906fb82edfcb
yancy <1014833992@163.com>
2018-03-09 20:52:21 +0800

    48_Rotate Image
    48_Rotate Image Solution

---
+++ 40-49/48_Rotate Image.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+#https://leetcode.com/problems/rotate-image/description/
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        for i in xrange(n / 2):
+            for j in xrange(i, n - i - 1):
+                tmp = matrix[i][j]
+                matrix[i][j] = matrix[n - j - 1][i]
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1]
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1]
+                matrix[j][n - i - 1] = tmp
